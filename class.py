64a30a1c222db99e1887c767ea63cd125f40ca5e
GOKULRAJ-B <149518589+GOKULRAJ-B@users.noreply.github.com>
2023-12-06 13:44:10 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,14 @@
+class student:
+    pass
+
+stud_1= student()
+
+stud_1.name = 'Raj'
+stud_1.roll = 15
+stud_1.sub='python'
+stud_1.mar= 100
+print('name:',stud_1.name)
+print('roll:',stud_1.roll)
+print('sub:', stud_1.sub)
+print('mar:', stud_1.mar)
+
